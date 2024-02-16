1c3aa57f5d110da9276853e8d7d33bf2cea78122
KdanielaP <135910567+KdanielaP@users.noreply.github.com>
2024-02-15 18:31:43 -0600

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ConsoleAPP1
+{
+
+    // Abstracción
+    public abstract class Animal
+    {
+        public abstract void EmitirSonido();
+    }
+
+    // Encapsulación
+    public class Perro : Animal
+    {
+        public override void EmitirSonido()
+        {
+            Console.WriteLine("Guau guau!");
+        }
+    }
+
+    public class Gato : Animal
+    {
+        public override void EmitirSonido()
+        {
+            Console.WriteLine("Miau miau!");
+        }
+    }
+
+    // Polimorfismo
+    public class Veterinario
+    {
+        public void Examinar(Animal animal)
+        {
+            Console.WriteLine("Examinando al animal...");
+            animal.EmitirSonido();
+        }
+    }
+
+    // Herencia
+    public class PerroCaliente : Perro
+    {
+        public void Servir()
+        {
+            Console.WriteLine("¡Listo para servir como un perro caliente!");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Perro perro = new Perro();
+            Gato gato = new Gato();
+
+            Veterinario veterinario = new Veterinario();
+            veterinario.Examinar(perro);
+            veterinario.Examinar(gato);
+
+            PerroCaliente perroCaliente = new PerroCaliente();
+            perroCaliente.EmitirSonido();
+            perroCaliente.Servir();
+        }
+    }
+}
